524f6a3d0ccd536dc28e32da7110279f5c82a9f0
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-03 22:10:08 +0200

    add barebones deno.land registry file

---
+++ src/features/deno-module-registries/deno.land.registry.ts
@@ -0,0 +1,16 @@
+import { DenoCommandShim } from "../deno-command-shim/deno-command-shim.type.ts";
+import { DenoModuleRegistry } from "./deno-module-registry.type.ts";
+
+export class DenoLandRegistry implements DenoModuleRegistry {
+  readonly hostname: string;
+
+  constructor() {
+    this.hostname = `deno.land`;
+  }
+
+  getLatestVersion(
+    commandShim: DenoCommandShim,
+  ): { versionString: string; latestVersionModuleUrl: URL } {
+    throw new Error("Method not implemented.");
+  }
+}
