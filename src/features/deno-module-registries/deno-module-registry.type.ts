9b97b8c10cda47ddbc73cf365ede462e5aeec827
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-03 21:56:04 +0200

    add a type for a deno-module-registry

---
+++ src/features/deno-module-registries/deno-module-registry.type.ts
@@ -0,0 +1,21 @@
+import { DenoCommandShim } from "../deno-command-shim/deno-command-shim.type.ts";
+/**
+ * Todo: Implement
+ */
+export interface DenoModuleRegistry {
+  /**
+   * The hostname of this deno registry.
+   * This is used to compare against the URL.host field to detect,
+   * which registry implementation needs to be used.
+   */
+  hostname: string;
+
+  /**
+   * @param commandShim The deno command shim for which to get the latest version
+   *        Provides detailed information, such as moduleBaseUrl, moduleName, installedVersion, etc.
+   * @returns an object containing the latest version as string and the module url for the latest version as URL.
+   */
+  getLatestVersion(
+    commandShim: DenoCommandShim,
+  ): { versionString: string; latestVersionModuleUrl: URL };
+}
