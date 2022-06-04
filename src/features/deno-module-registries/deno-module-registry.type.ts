30e623e377f31e0a2affae546d27b15795a7afb3
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-04 09:54:15 +0200

    switch DenoModuleRegistry from interface to abstract class to be able to use a static member for the hostname

--- src/features/deno-module-registries/deno-module-registry.type.ts
+++ src/features/deno-module-registries/deno-module-registry.type.ts
@@ -3,22 +3,24 @@ import { DenoModuleUrlSegments } from "./deno-module-url-segments.type.ts";
 /**
  * Todo: Implement
  */
-export interface DenoModuleRegistry {
+export abstract class DenoModuleRegistry {
   /**
    * The hostname of this deno registry.
    * This is used to compare against the URL.host field to detect,
    * which registry implementation needs to be used.
    */
-  readonly hostname: string;
+  static readonly hostname: string;
 
   /**
    * @param commandShim The deno command shim for which to get the latest version
    *        Provides detailed information, such as moduleBaseUrl, moduleName, installedVersion, etc.
    * @returns an object containing the latest version as string and the module url for the latest version as URL.
    */
-  getLatestVersion(
+  abstract getLatestVersion(
     commandShim: DenoCommandShim,
   ): { versionString: string; latestVersionModuleUrl: URL };
 
-  parseModuleUrlSegments(urlInput: string | URL): DenoModuleUrlSegments;
+  abstract parseModuleUrlSegments(
+    urlInput: string | URL,
+  ): DenoModuleUrlSegments;
 }
