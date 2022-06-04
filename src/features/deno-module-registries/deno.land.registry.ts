b8eb5e695619bada77dd3b616760add8a3340dfc
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-04 12:16:17 +0200

    adjust deno.land.registry to new imports and add moduleBaseUrl generator

--- src/features/deno-module-registries/deno.land.registry.ts
+++ src/features/deno-module-registries/deno.land.registry.ts
@@ -1,6 +1,6 @@
-import { DenoCommandShim } from "../deno-command-shim/deno-command-shim.type.ts";
+import { CommandShim } from "../deno-command-shim/command-shim.type.ts";
 import { DenoModuleRegistry } from "./deno-module-registry.type.ts";
-import { DenoModuleUrlSegments } from "./deno-module-url-segments.type.ts";
+import { ModuleUrlSegments } from "./module-url-segments.type.ts";
 
 export class DenoLandRegistry extends DenoModuleRegistry {
   static readonly hostname = `deno.land`;
@@ -10,34 +10,37 @@ export class DenoLandRegistry extends DenoModuleRegistry {
   }
 
   getLatestVersion(
-    commandShim: DenoCommandShim,
+    commandShim: CommandShim,
   ): { versionString: string; latestVersionModuleUrl: URL } {
     throw new Error("Method not implemented.");
   }
 
   parseModuleUrlSegments(
     urlInput: string | URL,
-  ): DenoModuleUrlSegments {
+  ): ModuleUrlSegments {
     const url = (typeof urlInput === "string") ? new URL(urlInput) : urlInput;
 
     // important: filename can be placed in some subfolder! (therefore the rest param for filepath)
     const [_, _denoX, moduleAndVersion, ...filepath] = url.pathname
       .split("/");
     const [moduleName, moduleVersion] = moduleAndVersion.split("@");
-    const filename = filepath.at(-1);
+    const entryFileName = filepath.at(-1);
 
-    if (filename === undefined) {
+    if (entryFileName === undefined) {
       throw new Deno.errors.NotFound(
         `parseModuleUrl could not detect the filename from the moduleUrl: ${url.toString()}`,
       );
     }
 
+    const moduleBaseURL = new URL(`/x/${moduleName}`, url);
+
     return {
       completeModuleURL: url,
       moduleName,
       moduleVersion,
-      filepath: filepath.join("/"),
-      filename,
+      moduleBaseURL,
+      entryFileName,
+      entryFilePath: filepath.join("/"),
     };
   }
 }
