ef3ee604f146ba9cd5b00ed1394a895d343bd713
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-04 10:15:51 +0200

    add function to detect a deno module registry based on a given module url

---
+++ src/features/deno-module-registries/detect-module-registry.ts
@@ -0,0 +1,22 @@
+import { DenoModuleRegistry } from "./deno-module-registry.type.ts";
+import { DenoLandRegistry } from "./deno.land.registry.ts";
+import { NestLandRegistry } from "./nest.land.registry.ts";
+import { UnknownRegistry } from "./unknown.registry.ts";
+
+/**
+ * Detects a deno module registry based on the url of a module
+ */
+export function detectModuleRegistry(
+  urlInput: string | URL,
+): DenoModuleRegistry {
+  const url = (typeof urlInput === "string") ? new URL(urlInput) : urlInput;
+
+  switch (url.hostname) {
+    case DenoLandRegistry.hostname:
+      return new DenoLandRegistry();
+    case NestLandRegistry.hostname:
+      return new NestLandRegistry();
+    default:
+      return new UnknownRegistry();
+  }
+}
