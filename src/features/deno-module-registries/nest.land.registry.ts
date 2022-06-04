76655f11e5cbcf8f9baecc141ab25ae16c97a5b5
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-04 15:06:49 +0200

    impleemtn getLatestVersion and parseModuleUrlSegments for nest.land registry

--- src/features/deno-module-registries/nest.land.registry.ts
+++ src/features/deno-module-registries/nest.land.registry.ts
@@ -10,14 +10,54 @@ export class NestLandRegistry extends DenoModuleRegistry {
     super();
   }
 
-  getLatestVersion(
-    moduleBaseURL: URL,
+  async getLatestVersion(
+    commandShim: CommandShim,
   ): Promise<LatestModuleVersion> {
-    throw new Error("Method not implemented.");
+    const moduleName = commandShim.moduleUrlSegments.moduleName;
+    const apiPackageUrl = `https://x.nest.land/api/package/${moduleName}`;
+    const res = await fetch(apiPackageUrl);
+
+    const { latestStableVersion } = await res.json();
+    const latestModuleVersion = latestStableVersion.split("@")[1];
+
+    return {
+      latestModuleVersion,
+      latestVersionModuleUrl: this.buildModuleUrl(
+        commandShim.moduleUrlSegments.moduleBaseURL,
+        latestModuleVersion,
+        commandShim.moduleUrlSegments.entryFilePath,
+      ),
+    };
   }
 
+  /**
+   * Example nest land url: https://x.nest.land/dmm@2.1.0/mod.ts
+   * CAUTION: does only work for fetching exact versions!
+   *
+   * @param urlInput
+   */
   parseModuleUrlSegments(urlInput: string | URL): ModuleUrlSegments {
-    throw new Error("Method not implemented.");
+    const url = (typeof urlInput === "string") ? new URL(urlInput) : urlInput;
+
+    const [_, moduleAndVersion, ...filepath] = url.pathname.split("/");
+    const [moduleName, moduleVersion] = moduleAndVersion.split("@");
+    const entryFileName = filepath.at(-1);
+    if (entryFileName === undefined) {
+      throw new Deno.errors.NotFound(
+        `parseModuleUrl could not detect the filename from the moduleUrl: ${url.toString()}`,
+      );
+    }
+
+    const moduleBaseURL = new URL(`/${moduleName}`, url);
+
+    return {
+      completeModuleURL: url,
+      moduleName,
+      moduleVersion,
+      moduleBaseURL,
+      entryFileName,
+      entryFilePath: filepath.join("/"),
+    };
   }
 
   buildModuleUrl(
@@ -25,6 +65,10 @@ export class NestLandRegistry extends DenoModuleRegistry {
     moduleVersion: string,
     entryFilePath: string,
   ): URL {
-    throw new Error("Method not implemented.");
+    const moduleBaseUrlString = moduleBaseURL.toString();
+
+    return new URL(
+      `${moduleBaseUrlString}@${moduleVersion}/${entryFilePath}`,
+    );
   }
 }
