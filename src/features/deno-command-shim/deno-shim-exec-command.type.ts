e0dc62ab1ce3a3358d37844770dc22aea99a7415
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-04 10:08:59 +0200

    migrate url segments parsing stuff into deno-module-url-segments type

--- src/features/deno-command-shim/deno-shim-exec-command.type.ts
+++ src/features/deno-command-shim/deno-shim-exec-command.type.ts
@@ -18,41 +18,4 @@ export interface DenoShimExecCommand {
    * Example: https://deno.land/x/mymodule@2.0.1/main.ts
    */
   moduleURL: URL;
-
-  /**
-   * Contains the deno module registry
-   */
-  moduleRegistry?: DenoModuleRegistry;
-
-  /**
-   * Example: Contains an URL instance with the URL:
-   * https://deno.land/x/mymodule (can be used to construct urls for new versions)
-   */
-  moduleBaseURL: URL;
-
-  /**
-   * The module name as it's present in the moduleBaseURL.
-   * Example: mymodule part from the url:
-   * https://deno.land/x/mymodule
-   */
-  moduleName: string;
-
-  /**
-   * Contains the version of the installed Module as string.
-   * If no version is present in the moduleUrl,
-   * this will contain the string "latest"
-   * Example:
-   * ModuleURL: https://deno.land/x/mymodule@2.0.1/main.ts
-   * moduleVersion: "2.0.1"
-   */
-  moduleVersion: string;
-
-  /**
-   * The path for the entryfile.
-   * Normally only the filename, but can be a longer path when entryFile is in some subfolder.
-   * Example:
-   * ModuleURL: https://deno.land/x/mymodule@3.0.1/dist/main.ts
-   * entryFilePath: "dist/main.ts"
-   */
-  entryFilePath: string;
 }
--- src/features/deno-module-registries/deno-module-url-segments.type.ts
+++ src/features/deno-module-registries/deno-module-url-segments.type.ts
@@ -1,7 +1,56 @@
+import { DenoModuleRegistry } from "./deno-module-registry.type.ts";
+
 export interface DenoModuleUrlSegments {
+  /**
+   * The complete URL for the executable module
+   * Example: https://deno.land/x/mymodule@2.0.1/main.ts
+   */
   completeModuleURL: URL;
+
+  /**
+   * Contains the deno module registry
+   */
+  moduleRegistry?: DenoModuleRegistry;
+
+  /**
+   * The module name as it's present in the moduleBaseURL.
+   * Example: mymodule part from the url:
+   * https://deno.land/x/mymodule
+   */
   moduleName: string;
+
+  /**
+   * Example: Contains an URL instance with the URL:
+   * https://deno.land/x/mymodule (can be used to construct urls for new versions)
+   */
+  moduleBaseURL: URL;
+
+  /**
+   * Contains the version of the installed Module as string.
+   * If no version is present in the moduleUrl,
+   * this will contain the string "latest"
+   * Example:
+   * ModuleURL: https://deno.land/x/mymodule@2.0.1/main.ts
+   * moduleVersion: "2.0.1"
+   */
   moduleVersion: string;
-  filepath: string;
-  filename: string;
+
+  /**
+   * The path for the entryfile.
+   * Normally only the filename, but can be a longer path when entryFile is in some subfolder.
+   * Example:
+   * ModuleURL: https://deno.land/x/mymodule@3.0.1/dist/main.ts
+   * entryFilePath: "dist/main.ts"
+   */
+  entryFilePath: string;
+
+  /**
+   * The last segment of entryFilePath.
+   * Example:
+   * entryFilePath: dist/main.ts
+   * entryFileName: "main.ts"
+   *
+   * If entryFilePath has no subfolders, entryFileName === entryFilePath.
+   */
+  entryFileName: string;
 }
