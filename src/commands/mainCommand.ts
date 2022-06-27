60797f4bdb2b1dcbe5a7785ce81da5b895fc3c67
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-28 01:07:53 +0200

    move targetVersion to arguments again because this is more ergonomic as cli

--- src/commands/mainCommand.ts
+++ src/commands/mainCommand.ts
@@ -2,10 +2,12 @@ import { Line } from "../deps/line.ts";
 import { HelpCommand } from "./HelpCommand.ts";
 
 export class MainCommand extends Line.MainCommand {
-  public signature = "modup [cliName] ";
+  public signature = "modup [cliName] [targetVersion]";
 
   public arguments = {
     "cliName": `The name of the cliCommand as installed by "deno install"`,
+    "targetVersion":
+      `A version tag which maches an available tag for the module on deno.land/x. Use Default: latest`,
   };
 
   public options = {
@@ -13,8 +15,6 @@ export class MainCommand extends Line.MainCommand {
       `Does not execute the update but outputs useful information about what it will do.`,
     "-f, --force":
       `forces the cli to ignore all security validations during an update.`,
-    "-t [value], --targetVersion [value]":
-      `A version tag which maches an available tag for the module on deno.land/x. Default: latest`,
   };
 
   public subcommands = [
@@ -23,9 +23,11 @@ export class MainCommand extends Line.MainCommand {
 
   public handle(): void {
     const cliName = this.argument(`cliName`);
-    const targetVersion = this.option(`--targetVersion`) ?? `latest`;
+    const targetVersion = this.argument(`targetVersion`) ?? `latest`;
     const dryRun = this.option(`--dryRun`) ?? false;
     const force = this.option(`--force`) ?? false;
+
+    console.log(cliName, targetVersion, dryRun, force);
 
     //TODO: Throw error when cliName is undefined!
   }
