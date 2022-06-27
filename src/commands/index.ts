700fc56c79b0a64f035c9a5ead13805cf39ede62
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-28 01:16:32 +0200

    delete old yarg dependencies

--- src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-// Follows the pattern at
-// https://github.com/yargs/yargs/blob/main/docs/advanced.md#example-command-hierarchy-using-indexmjs
-
-import * as mainCommand from "./mainCommand.ts";
-import * as helpCommand from "./helpCommand.ts";
-
-export const commands = [mainCommand, helpCommand];
