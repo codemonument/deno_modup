25993327c7df7ddc4f5155db26c0d05fa94af757
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-27 23:37:02 +0200

    add test for findCommandShim to return a path to a freshly installed command shim

--- src/features/deno-command-shim/find-command-shim.test.ts
+++ src/features/deno-command-shim/find-command-shim.test.ts
@@ -12,4 +12,25 @@ describe(`findCommandShim`, () => {
     const result = await findCommandShim("bob");
     assert(result.hasError());
   });
+
+  it(`should return a path for a command`, async () => {
+    const install = Deno.run({
+      cmd: [
+        "deno",
+        "install",
+        "--name",
+        "cowsay",
+        "https://deno.land/x/cowsay/cowsay.ts",
+      ],
+    });
+    await install.status();
+    install.close();
+    const res = await findCommandShim(`cowsay`);
+    assert(res.hasValue());
+    const uninstall = Deno.run({
+      cmd: ["deno", "uninstall", "cowsay"],
+    });
+    await uninstall.status();
+    uninstall.close();
+  });
 });
