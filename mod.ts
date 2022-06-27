e251e2ec62f6d093a5e83b282465814ee4ff83eb
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-27 23:39:18 +0200

    export find and parse command shim functions in mod.ts

--- mod.ts
+++ mod.ts
@@ -4,3 +4,9 @@ import { handler as mainCommandHandler } from "./src/commands/mainCommand.ts";
  * Re-Exports the main command from this cli tool as typescript module to be consumed by other deno scripts
  */
 export const modup = mainCommandHandler;
+
+/**
+ * Export useful command shim functions
+ */
+export * from "./src/features/deno-command-shim/find-command-shim.ts";
+export * from "./src/features/deno-command-shim/parse-command-shim.ts";
