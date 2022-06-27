ccc6967bf93d262bdfdb488eaeff036e850d2de0
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-27 23:45:03 +0200

    add import.meta.main guard to main.ts

--- main.ts
+++ main.ts
@@ -2,15 +2,22 @@ import { log } from "./src/deps/log.std.ts";
 import { runCli } from "./src/runCli.ts";
 import { VERSION } from "./VERSION.ts";
 
-try {
-  log.info(
-    `Running ${Deno.env.get("CLI_NAME")}
-     Version: ${VERSION} \n`,
-  );
+if (import.meta.main) {
+  try {
+    log.info(
+      `Running ${Deno.env.get("CLI_NAME")}
+       Version: ${VERSION} \n`,
+    );
 
-  // Main Command
-  await runCli(Deno.args);
-} catch (error) {
-  log.error(error);
-  Deno.exit();
+    // Main Command
+    await runCli(Deno.args);
+  } catch (error) {
+    log.error(error);
+    Deno.exit();
+  }
 }
+
+console.log(
+  `You imported this main.ts file, but it is intended to be used with 'deno install'!
+Import /mod.ts instead!`,
+);
