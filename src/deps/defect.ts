794cb093ec7c9123c09ff60a570fd604e1fef8a2
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-27 21:58:29 +0200

    add defekt package as dependency for better error management

---
+++ src/deps/defect.ts
@@ -0,0 +1,5 @@
+/**
+ * Custom errors made simple - From thenativeweb
+ * Better handling of exceptions by using Result wrappers and real custom errors
+ */
+export * from "https://cdn.skypack.dev/defekt@9.2.0?dts";
