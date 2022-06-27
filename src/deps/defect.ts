5120723c946aa81a662649f5d0a420896dd86cec
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-06-27 22:23:52 +0200

    add idea for simplified error() function

--- src/deps/defect.ts
+++ src/deps/defect.ts
@@ -3,3 +3,15 @@
  * Better handling of exceptions by using Result wrappers and real custom errors
  */
 export * from "https://cdn.skypack.dev/defekt@9.2.0?dts";
+
+// TODO: Try to figure out the typing
+// import { defekt, error } from "https://cdn.skypack.dev/defekt@9.2.0?dts";
+
+// type DefectType = ReturnType<typeof defekt>;
+// type ErrorParams = ConstructorParameters<DefectType>;
+
+// export function customError(clazz: DefectType, params: ErrorParams) {
+//   const errorInstance = new clazz(params);
+
+//   return error(errorInstance);
+// }
