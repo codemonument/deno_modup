bcc77c653284580037f8d2b6a54aff300d00e687
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-08-15 17:01:30 +0200

    add testing mock module dependency and upgrade all other testing deps to 0.152

--- src/deps/testing.std.ts
+++ src/deps/testing.std.ts
@@ -1,3 +1,4 @@
-export * from "https://deno.land/std@0.141.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.141.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.141.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.152.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.152.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.152.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.152.0/testing/mock.ts";
