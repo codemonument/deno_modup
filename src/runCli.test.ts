99d20420db1ab22226de58ee4f1e28481484d6ec
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-08-15 17:19:04 +0200

    rename script to update snapshot files

--- src/runCli.test.ts
+++ src/runCli.test.ts
@@ -1,18 +1,30 @@
 import {
-  assertObjectMatch,
   assertRejects,
+  assertSpyCall,
+  beforeEach,
   describe,
   it,
+  stub,
 } from "./deps/testing.std.ts";
 import { log } from "./deps/log.std.ts";
 import { runCli } from "./runCli.ts";
 
-describe(`runCli`, () => {
+describe.only(`runCli`, () => {
+  const exitStub = stub(
+    Deno,
+    "exit",
+    (code) => {
+      throw new Error(`Deno.exit() was called with exit code ${code}!`);
+    },
+  );
+
   it(`on arg '--help': showHelp & exit(0)`, async () => {
+    await assertSpyCall(exitStub, 0);
     await assertRejects(() => runCli(["--help"]));
   });
 
   it(`on command 'help': showHelp & exit(0)`, async () => {
-    await runCli(["help"]);
+    await assertSpyCall(exitStub, 0);
+    await assertRejects(() => runCli(["help"]));
   });
 });
